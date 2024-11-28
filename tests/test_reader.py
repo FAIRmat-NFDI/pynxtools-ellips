5abf7baa380e25ec04587f72dd584289848b2c85
Rubel <rubel.mozumder@outlook.com>
2024-11-28 16:16:37 +0100

    Implement test framework.

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,45 +1,24 @@
 """
-Basic example based test for the stm reader
+Basic example based test for the Ellips reader
 """
 
 import os
-import xml.etree.ElementTree as ET
-from glob import glob
 
-from pynxtools.dataconverter.helpers import (
-    generate_template_from_nxdl,
-    validate_data_dict,
+import pytest
+from pynxtools.testing.nexus_conversion import ReaderTest
+
+module_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+@pytest.mark.parametrize(
+    "nxdl,reader_name,files_or_dir",
+    [
+        ("NXellipsometry", "ellips", f"{module_dir}/data"),
+    ],
 )
-from pynxtools.dataconverter.template import Template
-from pynxtools.definitions.dev_tools.utils.nxdl_utils import get_nexus_definitions_path
-
-from pynxtools_ellips.reader import EllipsometryReader
-
-
-def test_example_data():
-    """
-    Test the example data for the stm reader
-    """
-    reader = EllipsometryReader
-    assert callable(reader.read)
-
-    def_dir = get_nexus_definitions_path()
-
-    data_dir = os.path.join(os.path.dirname(__file__), "data")
-    input_files = sorted(glob(os.path.join(data_dir, "*")))
-
-    for supported_nxdl in reader.supported_nxdls:
-        nxdl_file = os.path.join(
-            def_dir, "contributed_definitions", f"{supported_nxdl}.nxdl.xml"
-        )
-
-        root = ET.parse(nxdl_file).getroot()
-        template = Template()
-        generate_template_from_nxdl(root, template)
-
-        read_data = reader().read(
-            template=Template(template), file_paths=tuple(input_files)
-        )
-
-        assert isinstance(read_data, Template)
-        assert validate_data_dict(template, read_data, root)
+def test_stm_reader(nxdl, reader_name, files_or_dir, tmp_path, caplog):
+    "Generic test from pynxtools."
+    # test plugin reader
+    test = ReaderTest(nxdl, reader_name, files_or_dir, tmp_path, caplog)
+    test.convert_to_nexus(caplog_level="ERROR", ignore_undocumented=True)
+    test.check_reproducibility_of_nexus()
